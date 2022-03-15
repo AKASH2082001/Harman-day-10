917972b3255cad7df64325ae65118e3374f9a1cd
AKASH2082001 <akashbme208@gmail.com>
2022-03-15 00:27:15 -0700

    Harman day 10

---
+++ src/Helloworld.java
@@ -0,0 +1,16 @@
+public class Helloworld {
+
+    public static void main(String[] args) {
+
+        int a=20;
+
+        if(a%2==0)
+        {
+            System.out.println("The given number is even");
+        }
+        else
+        {
+            System.out.println("The given number is odd");
+        }
+    }
+}
